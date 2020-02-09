87fcaf51cd3a529396d406e1d3b402bad52641fc
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-02-09 15:21:22 +0530

    Ejected after this commit

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className='App'>
-      <h1>hello world!!!</h1>
+    <div data-test='component-app' className='App'>
+      <h1 data-test='counter-display'>The counter is currently {counter}</h1>
+      <button
+        data-test='increment-button'
+        onClick={() => setCounter(counter + 1)}
+      >
+        Increment counter
+      </button>
     </div>
   );
 };
--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { render } from '@testing-library/react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
@@ -8,6 +7,22 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test('render without error', () => {
   const wrapper = shallow(<App />);
-  // console.log(wrapper.debug());
-  // expect(wrapper).toBeTruthy();
+  const appComponent = wrapper.find("[data-test='component-app']");
+  // expect(appComponent.length).toBe(1);
+});
+
+test('render increment button', () => {
+  const wrapper = shallow(<App />);
+});
+
+test('render counter display', () => {
+  const wrapper = shallow(<App />);
+});
+
+test('counter start 0', () => {
+  const wrapper = shallow(<App />);
+});
+
+test('clicking button increments counter display', () => {
+  const wrapper = shallow(<App />);
 });

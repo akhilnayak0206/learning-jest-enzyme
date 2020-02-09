a02508ded4da1b995256bdcbaac58d94b51fc650
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-02-09 14:58:00 +0530

    enzyme jest added

--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('render without error', () => {
+  const wrapper = shallow(<App />);
+  // console.log(wrapper.debug());
+  // expect(wrapper).toBeTruthy();
 });
